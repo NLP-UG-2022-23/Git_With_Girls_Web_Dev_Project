7e7eacefd887a9a58a67e39a93c612a7958aada8
JKomosa <j.komosa.347@studms.ug.edu.pl>
2024-04-09 11:14:52 +0200

    CSS tidy up
    cleared out the styles.css file from stuff that wasn't used, moved it around to introduce order, added comments, corrected the cuisineList to contain images and not links to pages with images

--- quiz.js
+++ quiz.js
@@ -1,12 +1,12 @@
 const userSelections = {};
-const cuisineList = [{ "question_id": 'question0', "culture": 'Italian', "images": ["https://www.pexels.com/photo/delicious-spaghetti-with-bolognese-sauce-and-parmesan-cheese-7218637/", "https://www.pexels.com/photo/woman-taking-a-slice-of-pizza-17942040/"] },
-{ "question_id": 'question1', "culture": 'Polish', "images": ["https://www.pexels.com/photo/brown-cookies-on-brown-wooden-table-4084924/", "https://www.pexels.com/photo/delicious-potato-dumplings-with-fried-bacon-slices-on-round-plate-4202381/"] },
-{ "question_id": 'question2', "culture": 'Greek', "images": ["https://www.pexels.com/photo/fresh-tofu-and-tomatoes-on-a-bowl-10780004/", "https://unsplash.com/photos/a-plate-of-food-on-a-table-with-vegetables-zON7CMCQogQ"] },
-{ "question_id": 'question3', "culture": 'Japanese', "images": ["https://www.pexels.com/photo/cooked-food-on-brown-wooden-board-670705/", "https://www.pexels.com/photo/close-up-photo-of-mouth-watering-sashimi-628776/"] },
-{ "question_id": 'question4', "culture": 'Thai', "images": ["https://unsplash.com/photos/a-table-topped-with-plates-filled-with-food-rWq_kKjrOfo", "https://www.pexels.com/photo/pad-thai-in-white-ceramic-plate-12481161/"] },
-{ "question_id": 'question5', "culture": 'Chinese', "images": ["https://www.pexels.com/photo/soup-with-dimsums-and-vegetables-on-ceramic-bowl-5409015/", "https://www.pexels.com/photo/trays-with-delicious-asian-dumplings-with-chopsticks-placed-on-table-7364181/"] },
-{ "question_id": 'question6', "culture": 'French', "images": ["https://www.pexels.com/photo/top-view-of-a-charcuterie-5975493/", "https://www.pexels.com/photo/appetizing-escargots-dish-served-in-restaurant-4946442/"] },
-{ "question_id": 'question7', "culture": 'American', "images": ["https://unsplash.com/photos/burger-with-lettuce-and-tomatoes-on-white-ceramic-plate-MH_lBTvkvPM", "https://www.pexels.com/photo/person-holding-hotdog-4676419/"] }
+const cuisineList = [{"question_id":'question0', "culture":'Italian', "images":["https://images.pexels.com/photos/7218637/pexels-photo-7218637.jpeg", "https://images.pexels.com/photos/17942040/pexels-photo-17942040.jpeg"]}, 
+{"question_id":'question1', "culture":'Polish', "images":["https://images.pexels.com/photos/4084924/pexels-photo-4084924.jpeg", "https://images.pexels.com/photos/4202381/pexels-photo-4202381.jpeg"]}, 
+{"question_id":'question2', "culture":'Greek', "images":["https://images.pexels.com/photos/10780004/pexels-photo-10780004.jpeg", "https://plus.unsplash.com/premium_photo-1667215177072-6539146bc577"]}, 
+{"question_id":'question3', "culture":'Japanese', "images":["https://images.pexels.com/photos/670705/pexels-photo-670705.jpeg", "https://images.pexels.com/photos/628776/pexels-photo-628776.jpeg"]}, 
+{"question_id":'question4', "culture":'Thai', "images":["https://plus.unsplash.com/premium_photo-1669150852119-74b41d658be5", "https://images.pexels.com/photos/12481161/pexels-photo-12481161.jpeg"]}, 
+{"question_id":'question5', "culture":'Chinese', "images":["https://images.pexels.com/photos/5409015/pexels-photo-5409015.jpeg", "https://images.pexels.com/photos/7364181/pexels-photo-7364181.jpeg"]}, 
+{"question_id":'question6', "culture":'French', "images":["https://images.pexels.com/photos/5975493/pexels-photo-5975493.jpeg", "https://images.pexels.com/photos/4946442/pexels-photo-4946442.jpeg"]}, 
+{"question_id":'question7', "culture":'American', "images":["https://images.unsplash.com/photo-1602030638412-bb8dcc0bc8b0", "https://images.pexels.com/photos/4676419/pexels-photo-4676419.jpeg"]}
 ]
 
 //const randomElement = array[Math.floor(Math.random() * array.length)];
